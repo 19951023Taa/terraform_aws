1be09314c17c50226d3349f667238a2f600d8c66
19951023Taa <takamaaa777@gmail.com>
2023-03-11 16:23:25 +0900

    backend変更

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "app" {
-  ami = data.aws_ami.app_ami.id
-  instance_type = "t3.micro"
-  subnet_id = aws_subnet.public_subnet_1a.id
+  ami                         = data.aws_ami.app_ami.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
   associate_public_ip_address = true
 
-  vpc_security_group_ids = [ 
+  vpc_security_group_ids = [
     aws_security_group.app_ec2_sg.id,
     aws_security_group.opp_sg.id
-   ]
+  ]
 
-   key_name = aws_key_pair.this.key_name
+  key_name = aws_key_pair.this.key_name
 
   tags = {
     "Name" = "${var.project}-${var.env}-app-ec2"
--- main.tf
+++ main.tf
@@ -9,6 +9,11 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform.s3.tfstate"
+    key    = "terraform-dev.tfstate"
+    region = "ap-northeast-1"
+  }
 }
 
 # ---------------------------
