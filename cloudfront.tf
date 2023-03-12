fc280906b6897ad33044378519abc4ee972147fa
19951023Taa <takamaaa777@gmail.com>
2023-03-12 16:51:23 +0900

    elb_cloudfront作成

---
+++ cloudfront.tf
@@ -0,0 +1,51 @@
+resource "aws_cloudfront_distribution" "this" {
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "TEST Cloudfront"
+  price_class     = "PriceClass_All"
+
+  origin {
+    domain_name = aws_route53_record.this.fqdn
+    origin_id   = aws_lb.this.name
+
+    custom_origin_config {
+      origin_protocol_policy = "match-viewer"
+      origin_ssl_protocols   = ["TLSv1.2"]
+      http_port              = 80
+      https_port             = 443
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+
+    target_origin_id       = aws_lb.this.name
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type =  "none"
+    }
+  }
+
+  aliases = ["dev.${var.domain}"]
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.this_verginia.arn
+    minimum_protocol_version = "TLSv1.2_2019"
+    ssl_support_method       = "sni-only"
+
+  }
+}
--- route53.tf
+++ route53.tf
@@ -17,3 +17,15 @@ resource "aws_route53_record" "this" {
     evaluate_target_health = true
   }
 }
+
+
+resource "aws_route53_record" "this_cloudfront" {
+  zone_id = aws_route53_zone.this.id
+  name    = "dev.${var.domain}"
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.this.domain_name
+    zone_id                = aws_cloudfront_distribution.this.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
